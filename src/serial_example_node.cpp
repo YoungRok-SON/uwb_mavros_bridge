19da1a9f0ce02c2bcf416693f7713710a19b0f57
YoungRok-SON <dudfhr3349@gmail.com>
2023-07-11 20:15:19 +0900

    working code.

--- src/serial_example_node.cpp
+++ src/serial_example_node.cpp
@@ -1,21 +1,3 @@
-/***
- * This example expects the serial port has a loopback on it.
- *
- * Alternatively, you could use an Arduino:
- *
- * <pre>
- *  void setup() {
- *    Serial.begin(<insert your baudrate here>);
- *  }
- *
- *  void loop() {
- *    if (Serial.available()) {
- *      Serial.write(Serial.read());
- *    }
- *  }
- * </pre>
- */
-
 #include <iostream>
 #include <string>
 #include <serial/serial.h>
@@ -27,17 +9,11 @@
 
 serial::Serial ser;
 
-void write_callback(const std_msgs::String::ConstPtr& msg){
-    ROS_INFO_STREAM("Writing to serial port" << msg->data);
-    ser.write(msg->data);
-}
 
 int main (int argc, char** argv){
-    ros::init(argc, argv, "serial_example_node");
+    ros::init(argc, argv, "uwb_loc");
     ros::NodeHandle nh;
 
-    ros::Subscriber write_sub = nh.subscribe("write", 1000, write_callback);
-    ros::Publisher read_pub = nh.advertise<std_msgs::String>("read", 1000);
     ros::Publisher ros_pub_uwb_position = nh.advertise<geometry_msgs::PoseStamped>("/mavros/vision_pose/pose",1);
     ros::Rate loop_rate(40);
     try
@@ -67,6 +43,9 @@ int main (int argc, char** argv){
         ros::spinOnce();
 
         geometry_msgs::PoseStamped geo_pose_uwb_position;
+        geo_pose_uwb_position.header.stamp = ros::Time::now();
+        // Need to fix this orientation .. Almost Done !
+        geo_pose_uwb_position.header.frame_id = "/MAV_FRAME_LOCAL_ENU"; 
 
         if(ser.available()){
             
@@ -99,7 +78,7 @@ int main (int argc, char** argv){
             i_second_idx = result.data.find(str_second_seperator, i_init_loc) ;
             i_length     = i_second_idx - i_first_idx;
             float i_z_value    = stof(result.data.substr( i_first_idx , i_length ))/1000;
-            //std::cout << "X: " << i_x_value << " Y:  " << i_y_value << " Z: " << i_z_value << std::endl;
+            std::cout << "X: " << i_x_value << " Y:  " << i_y_value << " Z: " << i_z_value << std::endl;
 
             // Send to Mavros
             geo_pose_uwb_position.pose.position.x = i_x_value;
@@ -114,7 +93,8 @@ int main (int argc, char** argv){
         {
             geo_pose_uwb_position.pose.position.x = geo_pose_uwb_position_prev.pose.position.x;
             geo_pose_uwb_position.pose.position.y = geo_pose_uwb_position_prev.pose.position.y;
-            geo_pose_uwb_position.pose.position.z = geo_pose_uwb_position_prev.pose.position.z;            
+            geo_pose_uwb_position.pose.position.z = geo_pose_uwb_position_prev.pose.position.z;
+            std::cout << "X: " <<  geo_pose_uwb_position.pose.position.x  << " Y:  " <<  geo_pose_uwb_position.pose.position.y  << " Z: " <<  geo_pose_uwb_position.pose.position.z << " (Prev)" << std::endl;
         }
 
         // Publish PoseStamped data to mavros.
@@ -123,55 +103,5 @@ int main (int argc, char** argv){
 
     }
     
-    /* 
-    while(ros::ok()){
-
-        ros::spinOnce();
-
-        if(ser.available()){
-            
-            std_msgs::String result;
-            
-            result.data = ser.read(ser.available());
-            
-            // from std_msgs::String to geometry_msgs::PoseStamped
-            geometry_msgs::PoseStamped geo_pose_uwb_position;
-            //std::cout << result.data << std::endl;
-
-            int i_init_loc = 0;
-            // Find X value
-            std::string str_first_seperator  = ":";
-            std::string str_second_seperator = ",";
-
-            int i_first_idx  = result.data.find(str_first_seperator, i_init_loc) + 1;
-            int i_second_idx = result.data.find(str_second_seperator, i_init_loc) ;
-            int i_length     = i_second_idx - i_first_idx;
-            float i_x_value    = stof(result.data.substr( i_first_idx , i_length ))/1000;
-            i_init_loc = i_second_idx+1;  // add 1 for find next seperation point.
-            // Find Y value
-            i_first_idx  = result.data.find(str_first_seperator, i_init_loc) + 1;
-            i_second_idx = result.data.find(str_second_seperator, i_init_loc) ;
-            i_length     = i_second_idx - i_first_idx;
-            float i_y_value    = stof(result.data.substr( i_first_idx , i_length ))/1000;
-            i_init_loc = i_second_idx+1;  // add 1 for find next seperation point.
-            // Find Z value
-            str_second_seperator = "\n";
-            i_first_idx  = result.data.find(str_first_seperator, i_init_loc) + 1;
-            i_second_idx = result.data.find(str_second_seperator, i_init_loc) ;
-            i_length     = i_second_idx - i_first_idx;
-            float i_z_value    = stof(result.data.substr( i_first_idx , i_length ))/1000;
-            //std::cout << "X: " << i_x_value << " Y:  " << i_y_value << " Z: " << i_z_value << std::endl;
-
-            // Send to Mavros
-            geo_pose_uwb_position.pose.position.x = i_x_value;
-            geo_pose_uwb_position.pose.position.y = i_y_value;
-            geo_pose_uwb_position.pose.position.z = i_z_value;
-            read_pub.publish(result);
-            ros_pub_uwb_position.publish(geo_pose_uwb_position);
-            // Publish PoseStamped data to mavros.
-        }
-        loop_rate.sleep();
-
-    } */
 }
 
